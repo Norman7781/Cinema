c112e35469cbfc674409a0176917ced26fbb761f
Norman7781 <aungkgmyat1129@gmail.com>
2024-09-21 20:37:49 +0700

    Cinema project

--- CinemaTicketBookingSystem/src/com/cinema/database/PgSqlConnectionFactory.java
+++ CinemaTicketBookingSystem/src/com/cinema/database/PgSqlConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class PgSqlConnectionFactory {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/cinema_db";
+    private static final String URL = "jdbc:postgresql://localhost:5432/norman";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
